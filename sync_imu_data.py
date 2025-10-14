fc315de0bb90189a937ac18b042ab2f58bbd643e
alkvi <alexander.kvist@ki.se>
2025-10-14 10:34:43 +0200

    Add subj2 data, clean up files

--- sync_imu_data.py
+++ sync_imu_data.py
@@ -5,38 +5,23 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 from scipy.interpolate import interp1d
 
-def set_xvalues(polygon, x0, x1):
-    _ndarray = polygon.get_xy()
-    _ndarray[:, 0] = [x0, x0, x1, x1, x0]
-    polygon.set_xy(_ndarray)
-
-# def update(val):
-#     new_lag = lag_slider.val
-#     for i in range(0, len(stim_polygons)):
-#         poly = stim_polygons[i]
-#         onset = onsets[i]
-#         duration = durations[i]
-#         set_xvalues(poly, onset+new_lag, onset+new_lag+duration)
-#     fig.canvas.draw_idle()
-
 if __name__ == "__main__":
 
     # Where all the data is stored
-    pq_file = "data/apdm/apdm_subj1_outside_imu_data.parquet"
-    csv_file = "data/actigraph/actigraph_subj1_outside/data_acc.csv"
+    pq_file = "data/apdm/apdm_subj2_outside_imu_data.parquet"
+    csv_file = "data/actigraph/actigraph_subj2_outside/data_acc.csv"
 
     fs_opal = 128
     fs_actigraph = 30
 
-    imu_data = pd.read_parquet(pq_file)
-    imu_subject = list(imu_data.columns)[0].split("/")[0]
+    opal_imu_data = pd.read_parquet(pq_file)
 
     # Get acc data for session
     session = "test"
     seek_column = session + "/LUMBAR/Accelerometer"
-    acc_data = imu_data.filter(regex=seek_column).to_numpy()
+    opal_acc_data = opal_imu_data.filter(regex=seek_column).to_numpy()
     print("Using pq file " + pq_file)
-    print(imu_data.head())
+    print(opal_imu_data.head())
 
     # Actigraph acc
     actigraph_data = pd.read_csv(csv_file)
@@ -44,13 +29,13 @@ if __name__ == "__main__":
 
     # Create time axes
     t_actigraph = np.arange(0, len(actigraph_acc)) / fs_actigraph
-    t_opal = np.arange(0, len(acc_data)) / fs_opal
+    t_opal = np.arange(0, len(opal_acc_data)) / fs_opal
 
     #Plot in two subplots
     fig, axs = plt.subplots(2, 1, figsize=(14, 8), sharex=False)
-    axs[0].plot(t_opal, acc_data[:, 0], label='Opal X')
+    axs[0].plot(t_opal, opal_acc_data[:, 0], label='Opal X')
     axs[0].set_title('Opal Accelerometer')
-    axs[0].set_ylabel('Acceleration (g)')
+    axs[0].set_ylabel('Acceleration (m/s2)')
     axs[0].legend()
     axs[1].plot(t_actigraph, actigraph_acc[:, 0], label='ActiGraph X', color='orange')
     axs[1].set_title('ActiGraph Accelerometer')
@@ -73,7 +58,7 @@ if __name__ == "__main__":
 
     fig, ax = plt.subplots(figsize=(14, 6))
     plt.subplots_adjust(left=0.25, bottom=0.25)
-    line_opal, = ax.plot(t_opal, acc_data[:, 0], label='Opal')
+    line_opal, = ax.plot(t_opal, opal_acc_data[:, 0], label='Opal')
     line_acti, = ax.plot(t_opal, np.roll(actigraph_resampled, best_lag), label='ActiGraph')
     ax.legend()
 
